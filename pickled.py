6649b0078422fd5166a1b070dfbd5f52972b7729
Griffith Heller <griffith1213@gmail.com>
2017-07-12 16:01:22 -0400

    wednesday

---
+++ pickled.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from pickle import dump
+from pickle import load
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Activation
+
+inData = load(open('input.pkl' , 'rb' ))
+outData = load(open('output.pkl' , 'rb' ))
+print(inData)
+print(outData)
+for a in range(10):
+    i = 0
+    for b in range(16):
+        temp = ""
+        for c in range(16):
+            temp+=str(int(inData[a][i]))+" "
+            i+=1
+        print(temp)
+    print(outData[a][0])
+    print()
+
+model = Sequential()
+model.add(Dense(16, input_dim=256, activation='tanh', use_bias=True))
+model.add(Dense(200, activation='tanh'))
+model.add(Dense(100, activation='tanh'))
+model.add(Dense(1, activation='softmax'))
+
+model.compile(optimizer='adam',loss='mean_squared_error', metrics=['accuracy'])
+
+model.fit(inData, outData, epochs=100, verbose=2)
+
+print(model.predict(inData[3]))
